9edcbce7147184d47291ea7685df4d21f50afdb4
ete <ete0918@163.com>
2018-05-02 16:34:38 +0800

    商品订单更新

--- application/models/Goodsordermodel.php
+++ application/models/Goodsordermodel.php
@@ -10,4 +10,13 @@ class Goodsordermodel extends Basemodel
 {
     protected $table = 'boss_shop_order';
     protected $hidden = ['created_at','updated_at','deleted_at'];
+
+    public function customer(){
+        return $this->belongsTo(Customermodel::class,'customer_id')->select('id','name');
+    }
+    public function address()
+    {
+        return $this->belongsTo(Shopaddressmodel::class, 'address_id')
+                    ->select('id','apartment','building','room_number');
+    }
 }
---
+++ application/models/Shopaddressmodel.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      hfq<1326432154@qq.com>
+ * Date:        2018/5/2
+ * Time:        16:19
+ * Describe:    收货地址
+ */
+
+class Shopaddressmodel extends Basemodel
+{
+    protected $table = 'boss_shop_address';
+    protected $hidden = ['created_at','updated_at','deleted_at'];
+
+}
