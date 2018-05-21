84914ae2a22a0c9dd2662af14331a08469ea1129
chenkunkun <cooook@163.com>
2018-05-21 11:40:53 +0800

    新增职位

--- application/controllers/employee/Position.php
+++ application/controllers/employee/Position.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+use Illuminate\Database\Capsule\Manager as DB;
 /**
  * Author:      zjh<401967974@qq.com>
  * Date:        2018/5/14 0014
@@ -21,6 +22,36 @@ class Position extends MY_Controller
         $filed = ['name', 'pc_privilege', 'mini_privilege'];
         $category = $this->positionmodel->get($filed);
         $this->api_res(0, $category);
+    }
+
+    public function addPosition()
+    {
+        $post = $this->input->post(null,true);
+        $name = isset($post['name']) ? $post['name'] : null;
+        $pc_privilege = isset($post['pc_privilege']) ? $post['pc_privilege'] : null;
+        $mini_privilege = isset($post['mini_privilege']) ? $post['mini_privilege'] : null;
+
+        try{
+            DB::beginTransaction();
+            $result = positionmodel::insert(
+                [   'name' => $name,
+                    'pc_privilege' => $pc_privilege,
+                    'mini_privilege' => $mini_privilege,
+                    'created_at' => date('Y-m-d H:i:s',time()),
+                    'updated_at' => date('Y-m-d H:i:s',time())
+                ]);
+            if(!$result) {
+                DB::rollBack();
+                $this->api_res(1009);
+                return;
+            }
+            DB::commit();
+            $this->api_res(0);
+        }catch (Exception $e){
+            DB::rollBack();
+            throw $e;
+        }
+
     }
 
 
