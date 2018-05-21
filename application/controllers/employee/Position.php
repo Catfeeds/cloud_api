2489591a7ad7ff5046d807e4006ebda5e15be025
chenkunkun <cooook@163.com>
2018-05-21 14:38:29 +0800

    编辑职位

--- application/controllers/employee/Position.php
+++ application/controllers/employee/Position.php
@@ -19,9 +19,39 @@ class Position extends MY_Controller
 
     public function editPosition()
     {
-        $filed = ['name', 'pc_privilege', 'mini_privilege'];
-        $category = $this->positionmodel->get($filed);
-        $this->api_res(0, $category);
+        $post = $this->input->post(null,true);
+
+        if (isset($post['id']) && !empty($post['id'])) {
+            $position = Positionmodel::find($post['id']);
+            if (!$position) {
+                $this->api_res(1009);
+                return;
+            }
+            if (isset($post['name']) && !empty($post['name'])) {
+                if ($post['name'] == $position->name) {
+                    $pc_privilege = isset($post['pc_privilege']) ? $post['pc_privilege'] : null;
+                    $mini_privilege = isset($post['mini_privilege']) ? $post['mini_privilege'] : null;
+                    $position->pc_privilege = $pc_privilege;
+                    $position->mini_privilege = $mini_privilege;
+                    $result = $position->save();
+                    if (!$result) {
+                        $this->api_res(1009);
+                        return;
+                    }
+                    $this->api_res(0);
+                } else {
+                    $this->api_res(10101);
+                    return;
+                }
+            } else {
+                $category  = [$position->name, $position->pc_privilege, $position->mini_privilege];
+                $this->api_res(0, $category);
+            }
+        } else {
+            $this->api_res(10101);
+            return;
+        }
+
     }
 
     public function addPosition()
