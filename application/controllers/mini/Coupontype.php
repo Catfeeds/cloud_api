fa205e8ea527f646305ccd34fcd4f4fdff7d487a
dowell_funxdata <apsondu@gmail.com>
2018-05-25 19:32:39 +0800

    wws contract

---
+++ application/controllers/mini/Coupontype.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      zjh<401967974@qq.com>
+ * Date:        2018/5/25 0025
+ * Time:        14:37
+ * Describe:    优惠券类型
+ */
