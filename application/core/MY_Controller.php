d35b30b51152a1a44c1489439b0cfb11dedc3245
chenkunkun <cooook@163.com>
2018-06-08 14:06:41 +0800

    MY_Controller中保护变量删除

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,10 +9,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class MY_Controller extends CI_Controller {
 
-    protected $position;
-
-    protected $employee;
-
     public function __construct()
     {
         parent::__construct();
