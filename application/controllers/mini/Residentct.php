eec4cae09701a76715d44c70f99dae7a69ada0ab
chenkunkun <cooook@163.com>
2018-06-09 19:54:35 +0800

    住户中心

--- application/controllers/mini/Residentct.php
+++ application/controllers/mini/Residentct.php
@@ -172,13 +172,17 @@ class Residentct extends MY_Controller
      */
     public function displayCenter()
     {
-        /*$post = $this->input->post(null, true);
-        $field = ['id, name, position_id, store_id', 'avatar'];
+        $post = $this->input->post(null, true);
+        $field = ['id', 'name', 'position_id', 'store_id', 'avatar'];
         $this->load->model('employeemodel');
-        $employees = Employeemodel::with(['position' => function ($query) {
+        $this->load->model('positionmodel');
+        $employee = Employeemodel::with(['position' => function ($query) {
             $query->select('id', 'name');
-        }])->where('bxid', CURRENT_ID)->get($field);
-        $this->load->model('storemodel');*/
+        }])->where('bxid', CURRENT_ID)->first($field);
+        $this->load->model('storemodel');
+        $store = Storemodel::where('id', $employee->store_id)->first(['name']);
+        $employee->store_name = $store->name;
+        $this->api_res(0, ['data' => $employee]);
     }
 
     /**
