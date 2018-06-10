b971696ebfdfb2d8214634257169be495c8d69ad
zhangjunhu <401967974@qq.com>
2018-06-10 16:39:45 +0800

    '合同签署回调'

---
+++ application/controllers/mini/Contract.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      zjh<401967974@qq.com>
+ * Date:        2018/6/10 0010
+ * Time:        16:38
+ * Describe:
+ */
+class Contract extends MY_Controller{
+
+    public function notify(){
+        log_message('error','FDD合同签署回调成功');
+    }
+}
