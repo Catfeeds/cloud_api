45714f5b3941107e5d1b136091d8836714a58edf
chenkunkun <cooook@163.com>
2018-06-16 17:40:12 +0800

    'dowell'

--- application/controllers/bill/Order.php
+++ application/controllers/bill/Order.php
@@ -66,7 +66,7 @@ class Order extends MY_Controller
                 });
             })
             ->where($where)
-            ->orderBy('updated_at')->offset($offset)->limit(PAGINATE)
+            ->orderBy('created_at')->offset($offset)->limit(PAGINATE)
             ->get()->toArray();
 
 
