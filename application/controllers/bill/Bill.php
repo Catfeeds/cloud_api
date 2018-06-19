5446045dcc28841c570997ab9bc65b28704741e6
chenkunkun <cooook@163.com>
2018-06-19 14:48:03 +0800

    'dowell'

--- application/controllers/bill/Bill.php
+++ application/controllers/bill/Bill.php
@@ -118,14 +118,48 @@ class Bill extends MY_Controller
 
     public function test(){
 
+        $this->load->model('roomtypemodel');
         $this->load->model('residentmodel');
-
-        $one=Residentmodel::find(16);
-//        $data['res']=$one->begin_time;
-        echo substr($one['begin_time'],0,7);
-
-        $this->api_res(0,$one);
-
+        $this->load->model('ordermodel');
+        $post = $this->input->post(null,true);
+        $where      = [];
+        $post['store_id']=1;
+        if(!empty($post['building_id'])){$where['building_id'] = intval($post['building_id']);};
+        if(!empty($post['status'])){$where['status'] = trim($post['status']);};
+        if(!empty($post['store_id'])){$where['store_id'] = intval($post['store_id']);}
+        if(!empty($post['number'])){$where['number'] = trim($post['number']);}
+        $filed      = ['id','layer','status','room_type_id','number','rent_price','resident_id'];
+        $roomunion  = new Roomunionmodel();
+        if (!empty($post['BLANK_days'])){
+            $days = $post['BLANK_days'];
+            $where['status'] = "BLANK";
+            switch ($days){
+                case 1:
+                    $time = [date('Y-m-d H:i:s',strtotime('-10 day',time())),date('Y-m-d H:i:s',time())];
+                    $list = $roomunion->room_details($where,$filed,$time);
+                    break;
+                case 2:
+                    $time = [date('Y-m-d H:i:s',strtotime('-20 day',time())),date('Y-m-d H:i:s',strtotime('-10 day',time()))];
+                    $list = $roomunion->room_details($where,$filed,$time);
+                    break;
+                case 3;
+                    $time = [date('Y-m-d H:i:s',strtotime('-30 day',time())),date('Y-m-d H:i:s',strtotime('-20 day',time()))];
+                    $list = $roomunion->room_details($where,$filed,$time);
+                    break;
+                case 4:
+                    $time = [date('Y-m-d H:i:s',0),date('Y-m-d H:i:s',time())];
+                    $list = $roomunion->room_details($where,$filed,$time);
+                    break;
+                default:
+                    $time = [date('Y-m-d H:i:s',0),date('Y-m-d H:i:s',time())];
+                    $list = $roomunion->room_details($where,$filed,$time);
+                    break;
+            }
+        }else{
+            $time = [date('Y-m-d H:i:s',0),date('Y-m-d H:i:s',time())];
+            $list = $roomunion->room_details($where,$filed,$time);
+        }
+        $this->api_res(0,$list);
     }
 
 
