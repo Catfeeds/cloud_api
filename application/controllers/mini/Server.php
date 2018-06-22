2bcee4a57d8f60212f5960cb02276d06bc1cad9c
ete <ete0918@163.com>
2018-06-22 10:58:42 +0800

    'server'

--- application/controllers/mini/Server.php
+++ application/controllers/mini/Server.php
@@ -25,11 +25,16 @@ class Server extends MY_Controller
         $this->load->model('customermodel');
         $post   = $this->input->post(NULL, true);
         $number = !empty($post['number'])?trim($post['number']):null;
-        $room_id= Roomunionmodel::where('numbewr',$number)->get(['room_id'])->toArray();
+        $room_id= Roomunionmodel::where('number',$number)->get(['id'])->toArray();
+        if ($room_id){
+            $room_id = $room_id[0]['id'];
+        }else{
+            $this->api_res(1009);
+        }
         $filed  = ['id','room_id','customer_id','type','name', 'phone', 'time','deal', 'remark','status'];
         $store_id   = $this->employee->store_id;
         $server = Serviceordermodel::with('roomunion','customer')
-                                    ->where('store_id',$store_id)->where('')
+                                    ->where('store_id',$store_id)->where('room_id',$room_id)
                                     ->orderBy('id', 'desc')
                                     ->get($filed)->toArray();
         $this->api_res(0, ['list' => $server]);
