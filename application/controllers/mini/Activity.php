10bd3bc2ba95abd7f887049a4d0b210eb687f13d
ete <ete0918@163.com>
2018-06-27 17:41:09 +0800

    'coupon'

--- application/controllers/mini/Activity.php
+++ application/controllers/mini/Activity.php
@@ -17,24 +17,27 @@ class Activity extends MY_Controller
      * 展示当前公寓下的活动 （按类型分类）
      */
     public function showActivity(){
-        $input  = $this->input->post(null,true);
-        $type   = isset($input['type'])?$input['type']:'';
+        //$input  = $this->input->post(null,true);
+        //$type   = isset($input['type'])?$input['type']:'';
         $this->load->model('employeemodel');
         $this->load->model('storemodel');
         $this->load->model('activitymodel');
         $this->load->model('coupontypemodel');
         //$activities = $this->employee->store->activities;
-        $activities = Storemodel::find(1)->activities;
+        //$activities = Storemodel::find(1)->activities;
         //var_dump($activities->toArray());exit;
-        if ($type AND in_array($type, $this->activitymodel->getAllTypes())) {
+        /*if ($type AND in_array($type, $this->activitymodel->getAllTypes())) {
             $activities     = $activities->where('type', $type);
-        }
-        $list   = $activities->map(function($activity){
+        }*/
+       /* $list   = $activities->map(function($activity){
             if(Activitymodel::TYPE_DISCOUNT==$activity->type){
                 $activity->discount = $activity->coupontypes->first()->discount;
             }
             return $activity;
-        });
+        });*/
+
+       //$filed = [];
+       $list = Coupontypemodel::where('type','DISCOUNT')->get()->toArray();
 
         $this->api_res(0,['list'=>$list]);
     }
