83208219e3c21b4d4e1e1a2bef93a6a24258a0f5
ete <ete0918@163.com>
2018-06-28 18:14:59 +0800

    'ordercount'

--- application/controllers/utility/Utility.php
+++ application/controllers/utility/Utility.php
@@ -25,16 +25,23 @@ class Utility extends MY_Controller
         $this->load->model('buildingmodel');
         $this->load->model('roomunionmodel');
         $post = $this->input->post(null,true);
+        $page  = isset($post['page'])?intval($post['page']):1;
+        $offset= PAGINATE * ($page - 1);
         $where      = [];
         if(!empty($post['building_id'])){$where['building_id'] = intval($post['building_id']);};
         if(!empty($post['status'])){$where['status'] = trim($post['status']);};
         if(!empty($post['store_id'])){$where['store_id'] = intval($post['store_id']);}
         if(!empty($post['number'])){$number = trim($post['number']);}
-        $filed      = ['id','store_id','building_id','room_id','type','last_reading','this_reading','updated_at'];
-        $utility = Meterreadingtransfermodel::where($where)->orderBy('updated_at','DESC')
-                    ->with('store','building','roomunion')
-                    ->get($filed)->toArray();
-
-        $this->api_res(0,$utility);
+        $filed  = ['id','store_id','building_id','room_id','type','last_reading','this_reading','updated_at'];
+        $count  = ceil(Meterreadingtransfermodel::where($where)->count()/PAGINATE);
+        if ($page>$count||$page<1){
+            $this->api_res(0,['list'=>[]]);
+            return;
+        }else {
+            $utility = Meterreadingtransfermodel::where($where)->orderBy('updated_at', 'DESC')
+                ->with('store', 'building', 'roomunion')
+                ->get($filed)->toArray();
+            $this->api_res(0, ['list'=>$utility,'count'=>$count]);
+        }
     }
 }
