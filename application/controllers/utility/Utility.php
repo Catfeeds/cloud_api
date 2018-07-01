215b137a9626b70db55aad4c92722587a5a9d22c
ete <ete0918@163.com>
2018-07-02 06:55:06 +0800

    'utility'

--- application/controllers/utility/Utility.php
+++ application/controllers/utility/Utility.php
@@ -33,7 +33,7 @@ class Utility extends MY_Controller
         if(!empty($post['building_id'])){$where['building_id'] = intval($post['building_id']);};
         //if(!empty($post['status'])){$where['status'] = trim($post['status']);};
         if(!empty($post['store_id'])){$where['store_id'] = intval($post['store_id']);}
-        if(!empty($post['status'])){$where['comfired'] = intval($post['status']);}
+        if(!empty($post['status'])){$where['comfirmed'] = intval($post['status']);}
         if(!empty($post['type'])){$where['type'] = $post['type'];}
         $filed  = ['id','store_id','building_id','room_id','type','last_reading','last_time','this_reading','updated_at','confirmed'];
         $count  = ceil(Meterreadingtransfermodel::where($where)->count()/PAGINATE);
