b7fe0ce6f898db17926378a1772e6fa0dd1a1130
ete <ete0918@163.com>
2018-07-04 11:52:32 +0800

    'contract'

--- application/controllers/contract/Contract.php
+++ application/controllers/contract/Contract.php
@@ -42,7 +42,7 @@ class Contract extends MY_Controller
         };
         if(!empty($post['begin_time'])){$bt=$post['begin_time'];}else{$bt = date('Y-m-d H:i:s',0);};
         if(!empty($post['end_time'])){$et=$post['end_time'];}else{$et = date('Y-m-d H:i:s',time());};
-        $count = ceil(Contractmodel::where($where)/*->whereIn('resident_id',$resident_ids)*/->whereBetween('created_at',[$bt,$et])->count()/PAGINATE);
+        $count = ceil(Contractmodel::where($where)->whereIn('resident_id',$resident_ids)->whereBetween('created_at',[$bt,$et])->count()/PAGINATE);
         if ($page>$count||$page<1){
             $this->api_res(0,['list'=>[]]);
             return;
