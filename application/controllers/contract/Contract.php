ddb4eeea2cf30fe36f4fa1c03c0abf1a70d45bdc
ete <ete0918@163.com>
2018-07-04 16:47:06 +0800

    'contract'

--- application/controllers/contract/Contract.php
+++ application/controllers/contract/Contract.php
@@ -58,7 +58,7 @@ class Contract extends MY_Controller
             var_dump($resident_ids);
             $order = Contractmodel::with('employee')->with('resident')->with('store')->with('roomunion')
                 ->where($where)
-                ->whereBetween('created_at',[$bt,$et])//->where('resident_id',$resident_ids)
+                ->whereBetween('created_at',[$bt,$et])->where('resident_id',$resident_ids)
                 ->take(PAGINATE)->skip($offset)
                 ->orderBy('id','desc')->get($filed)->toArray();
         }
