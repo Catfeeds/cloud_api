99b6b4511291976bf225acaa173a92f4d14bb7d8
ete <ete0918@163.com>
2018-07-08 16:09:48 +0800

    'billexcel'

--- application/controllers/bill/Bill.php
+++ application/controllers/bill/Bill.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 use Carbon\Carbon;
 use EasyWeChat\Foundation\Application;
 use Illuminate\Database\Capsule\Manager as DB;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 /**
  * Author:      zjh<401967974@qq.com>
  * Date:        2018/5/29 0029
@@ -163,7 +165,115 @@ class Bill extends MY_Controller
         $this->api_res(0,$list);
     }
 
+    /**
+     * 导出流水
+     */
+    public function billExcel()
+    {
+        $post = $this->input->post(null,true);
+        $store_id   = trim($post['store_id']);
+        $begin      = empty($post['begin_time'])?date('Y-m-d H:i:s',0):trim($post['begin_time']);
+        $end        = empty($post['end_time'])?date('Y-m-d H:i:s',time()):trim($post['end_time']);
+        $filed      = ['id','store_id','employee_id','resident_id','room_id', 'money','type','pay_type',
+                        'pay_date','status','sequence_number','remark'];
+        if (!isset($post['store_id'])||empty($post['store_id'])){
+            $this->api_res(1002,[]);
+            return;
+        }
+        $this->load->model('billmodel');
+        $this->load->model('roomunionmodel');
+        $this->load->model('storemodel');
+        $this->load->model('residentmodel');
+        $this->load->model('employeemodel');
+        $bill = Billmodel::with(['roomunion_s','store_s','resident_s','employee_s','order'])
+                ->where('store_id',$store_id)
+                ->whereBetween('pay_date',[$begin,$end])->orderBy('pay_date','DESC')
+                ->get($filed)->toArray();
+        $bill_excel = [];
+        foreach ($bill as $key=>$value){
+            $res = [];
+            $res['pay_date']    = $bill[$key]['pay_date'];
+            $res['number']      = $bill[$key]['roomunion_s']['number'];
+            $res['resident']    = $bill[$key]['resident_s']['name'];
+            $res['money']       = $bill[$key]['money'];
+            $res['pay_type']    = $bill[$key]['pay_type'];
+            $res['ROOM_money']          = '';
+            $res['MANAGEMENT_money']    = '';
+            $res['DEPOSIT_R_money']     = '';
+            $res['DEPOSIT_O_money']     = '';
+            $res['WATER_money']         = '';
+            $res['HOT_WATER_money']     = '';
+            $res['ELECTRICITY_money']   = '';
+            $res['REFUND_money']        = '';
+            $res['other_money']         = '';
+            $res['remark']              = $bill[$key]['remark'];;
 
+            if(!empty($bill[$key]['order'])){
+                $order = $bill[$key]['order'];
+                $res['other_money'] = 0;
+                foreach ($order as $key_o=>$value_o){
+                    if($order[$key_o]['type']=='ROOM'){
+                        $res['ROOM_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='MANAGEMENT'){
+                        $res['MANAGEMENT_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='DEPOSIT_R'){
+                        $res['DEPOSIT_R_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='DEPOSIT_O'){
+                        $res['DEPOSIT_O_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='WATER'){
+                        $res['WATER_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='HOT_WATER'){
+                        $res['HOT_WATER_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='ELECTRICITY'){
+                        $res['ELECTRICITY_money'] = $order[$key_o]['paid'];
+                    }
+                    elseif($order[$key_o]['type']=='REFUND'){
+                        $res['REFUND_money'] = $order[$key_o]['paid'];
+                    }
+                    else{
+                        $res['other_money'] += $order[$key_o]['paid'];
+                    }
+                }
+            }
+            $bill_excel[]       = $res;
+        }
+        $objPHPExcel    = new Spreadsheet();
+        $sheet  = $objPHPExcel->getActiveSheet();
+        $i = 1;
+        $objPHPExcel->getActiveSheet()->setCellValue('A'.$i , '支付时间');
+        $objPHPExcel->getActiveSheet()->setCellValue('B'.$i , '房间号');
+        $objPHPExcel->getActiveSheet()->setCellValue('C'.$i , '住户姓名');
+        $objPHPExcel->getActiveSheet()->setCellValue('D'.$i , '支付总金额');
+        $objPHPExcel->getActiveSheet()->setCellValue('E'.$i , '支付方式');
+        $objPHPExcel->getActiveSheet()->setCellValue('F'.$i , '房租');
+        $objPHPExcel->getActiveSheet()->setCellValue('G'.$i , '物业');
+        $objPHPExcel->getActiveSheet()->setCellValue('H'.$i , '住宿押金');
+        $objPHPExcel->getActiveSheet()->setCellValue('I'.$i , '其他押金');
+        $objPHPExcel->getActiveSheet()->setCellValue('J'.$i , '水费');
+        $objPHPExcel->getActiveSheet()->setCellValue('K'.$i , '热水费');
+        $objPHPExcel->getActiveSheet()->setCellValue('L'.$i , '电费');
+        $objPHPExcel->getActiveSheet()->setCellValue('M'.$i , '退租');
+        $objPHPExcel->getActiveSheet()->setCellValue('N'.$i , '其它费用');
+        $objPHPExcel->getActiveSheet()->setCellValue('O'.$i , '备注');
+        $sheet->fromArray($bill_excel,null,'A2');
+        $writer = new Xlsx($objPHPExcel);
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
+        header("Content-Type:application/force-download");
+        header("Content-Type:application/vnd.ms-excel");
+        header("Content-Type:application/octet-stream");
+        header("Content-Type:application/download");;
+        header('Content-Disposition:attachment;filename="meterReadingTemplate.xlsx"');
+        header("Content-Transfer-Encoding:binary");
+        $writer->save('php://output');
+    }
 
     /********************************************生成账单******************************************/
 
