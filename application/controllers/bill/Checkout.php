d9b58b204f6e3723bfe38df02aa3d3cf39763d68
ete <ete0918@163.com>
2018-07-09 10:47:42 +0800

    'checkout_authority'

--- application/controllers/bill/Checkout.php
+++ application/controllers/bill/Checkout.php
@@ -30,7 +30,7 @@ class Checkout extends MY_Controller
         empty($input['store_id'])?:$where['store_id']=$input['store_id'];
         empty($input['type'])?:$where['type']=$input['type'];
         $store_ids = explode(',',$this->employee->store_ids);
-        $query   = Checkoutmodel::with('roomunion','store','resident')->whereIn('store_id',$store_ids)
+        $query   = Checkoutmodel::with('roomunion','store','resident')
             ->where($where);
 
         if(!empty($input['search']))
