3b9b08c773fda38b96f6c3dce2d062a237583963
zhangjunhu <401967974@qq.com>
2018-07-12 15:03:40 +0800

    'nothing'

--- application/controllers/mini/Resident.php
+++ application/controllers/mini/Resident.php
@@ -745,9 +745,9 @@ class Resident extends MY_Controller
                     $resident->orders()->delete();
                     //删除住户信息
                     $resident->delete();
+                    $resident->roomunion->update(['people_count'=>0,'resident_id'=>0,'status'=>Roomunionmodel::STATE_BLANK]);
                 }
             }
-            $resident->roomunion->update(['people_count'=>0,'resident_id'=>0,'status'=>Roomunionmodel::STATE_BLANK]);
             DB::commit();
             $this->api_res(0);
         }catch (Exception $e){
