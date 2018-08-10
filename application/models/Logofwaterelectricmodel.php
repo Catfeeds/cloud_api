fa49ccd664a92da0ba46602cee431fed1206c02d
shift_del <hfq@funxdata.com>
2018-08-10 12:03:45 +0800

    换表逻辑1.0

--- application/models/Logofwaterelectricmodel.php
+++ application/models/Logofwaterelectricmodel.php
@@ -9,6 +9,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Logofwaterelectricmodel extends Basemodel {
     protected $table = 'boss_log_water_electricity';
 
+    protected $fillable = ['transfer_id','employee_id','original_record','now_record','reason'];
+
     /**
      * 该记录所属房间
      */
