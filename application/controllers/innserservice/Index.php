033b19043103bf91b82923224cc46da8573fff3d
Wei Jinlong <4847327@qq.com>
2018-08-16 18:04:04 +0800

    demo

--- application/controllers/innserservice/Index.php
+++ application/controllers/innserservice/Index.php
@@ -22,5 +22,20 @@ class Index extends MY_Controller
         echo 'ok->'.X_API_TOKEN;
     }
 
+    //调用demo
+    public function client()
+    {
+        $apikey='111111111';
+        $apisecret='nf239fh293hf8h23f';
+        $timestamp=time();
 
+        $hash=$apihash = hash('sha256',"$apikey.$timestamp.$apisecret");
+        $x_api_token = "$apikey.$timestamp.$hash";
+
+        //调用curl
+        //此处省略初始化代码
+        $header[0] = "content-type: application/x-www-form-urlencoded;charset=UTF-8"; 
+        $header[] = "x-api-token: $x_api_token"; 
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    }
 }
