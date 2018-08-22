b445d831ddf37a74bf64229f361e0ec8355aef86
李若愚 <997596184@qq.com>
2018-08-22 15:20:11 +0800

    '活动添加详情页'

--- application/controllers/activity/Activity.php
+++ application/controllers/activity/Activity.php
@@ -110,23 +110,28 @@ class Activity extends MY_Controller
      * */
     private function ac_up($activity){
         foreach ($activity as $key => $coupon) {
-            $prize = Activityprizemodel::where('id', $coupon['prize_id'])->select(['prize'])->first();
+            $prize = Activityprizemodel::where('id', $coupon['prize_id'])->select(['prize','count'])->first();
             $p = unserialize($prize->prize);
+            $c = unserialize($prize->count);
             $couponarr = Coupontypemodel::whereIn('id', $p)->get(['name'])->toArray();
             $str = '';
             foreach ($couponarr as $value) {
                 $str .= $value['name'] . '/';
             }
-            $participate = Drawmodel::where('activity_id', $coupon['id'])->orderby('costomer_id')->count();
+            if($coupon['activity_type'] == 'OLDBELTNEW' || $coupon['activity_type'] == 'CHECKIN'){
+                $Lottery_number = Couponmodel::where('activity_id', $coupon['id'])->groupBy('resident_id')->count();
+                $lucky_draw = Couponmodel::where('activity_id',$coupon['id'])->count();
+            }ELSE{
             $Lottery_number = Drawmodel::where('activity_id', $coupon['id'])->count();
             $lucky_draw = Drawmodel::where(['activity_id' => $coupon['id'], 'is_draw' => '1'])->count();
+            }
             $employee_name = Employeemodel::where('id', $coupon['employee_id'])->first(['name']);
             $store_id = Storeactivitymodel::where('activity_id',$coupon['id'])->with('store')->get(['store_id'])->toarray();
             $store_str = '';
             foreach ($store_id as $value) {
                 $store_str .= $value['store']['name'] . '/';
             }
-            $resident = Couponmodel::where('activity_id',$coupon['id'])->groupBy('resident_id')->count();
+
             $coupon_count = Couponmodel::where('activity_id',$coupon['id'])->count();
             $data[$key]['id'] = $coupon['id'];
             $data[$key]['user'] = $employee_name->name;
@@ -134,13 +139,12 @@ class Activity extends MY_Controller
             $data[$key]['start_time'] = date("Y-m-d H:i", strtotime($coupon['start_time']));
             $data[$key]['end_time'] = date("Y-m-d H:i", strtotime($coupon['end_time']));
             $data[$key]['prize'] = $str;
+            $data[$key]['count'] = $c;
             $limit = unserialize($coupon['limit']);
             $data[$key]['customer'] = $limit['com'];
-            $data[$key]['resident'] = $resident;
             $data[$key]['coupon_count'] = $coupon_count;
             $data[$key]['type'] = $coupon['activity_type'];
             $data[$key]['limit'] = $limit['limit'];
-            $data[$key]['participate'] = $participate;
             $data[$key]['Lottery_number'] = $Lottery_number;
             $data[$key]['lucky_draw'] = $lucky_draw;
             $data[$key]['share_img'] = $coupon['share_img'];
