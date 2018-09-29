4161339d0e273ba5c592e16d5022d9fdd9a8480b
zhangjunhu <401967974@qq.com>
2018-09-29 18:01:21 +0800

    小程序使用预存金

---
+++ application/models/Premoneymodel.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      zjh<401967974@qq.com>
+ * Date:        2018/9/29 0029
+ * Time:        11:38
+ * Describe:    用户预存金
+ */
+class Premoneymodel extends Basemodel
+{
+    protected $table    = 'boss_pre_money';
+
+}
