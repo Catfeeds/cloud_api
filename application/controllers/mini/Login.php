ebcb31ecb473f214354372e9f5f7931f83e41338
黄芳强 <hfq@funxdata.com>
2018-09-30 10:04:48 +0000

    小程序登录

--- application/controllers/mini/Login.php
+++ application/controllers/mini/Login.php
@@ -9,58 +9,77 @@ use EasyWeChat\Foundation\Application;
  * Time:        14:06
  * Describe:
  */
-class Login extends MY_Controller {
-    protected $app;
-    protected $oauth;
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('employeemodel');
-        $this->load->helper('common');
-        $this->app = (new Application(getMiniWechatConfig()))->mini_program;
-    }
-
-    public function getToken() {
-        $post = $this->input->post(NULL, true);
-        if ($post['code']) {
-            $sessionKeyData = $this->app->sns->getSessionKey($post['code']);
-            $token          = $this->handleLoginStatus($sessionKeyData);
-            $this->api_res(0, ['token' => $token, '$sessionKeyData' => $sessionKeyData]);
-        } else {
-            $this->api_res(10002);
-            return;
-        }
-    }
-
-    public function handleLoginStatus($sessionKeyData) {
-        $this->load->library('M_jwt');
-        if (!isset($sessionKeyData->unionid)) {
-            $this->api_res(10002);
-            return;
-        }
-        $wechat = Employeemodel::where('unionid', $sessionKeyData->unionid)->first();
-
-        if (empty($wechat) OR $wechat->status == 'DISABLE') {
-            $this->api_res(10002);
-            return;
-        }
-        $wechat->mini_openid = $sessionKeyData->openid;
-        $wechat->session_key = $sessionKeyData->session_key;
-        $wechat->save();
-        return $this->m_jwt->generateJwtToken($wechat['bxid'], $wechat['company_id']);
-    }
-
-    public function authority() {
-        $this->load->model('storemodel');
-        //获取门店列表
-        $store_ids = $this->employee_store->store_ids;
-        if (empty($store_ids) || !isset($store_ids)) {
-            $this->api_res(1018);
-            return;
-        }
-        $where = ['company_id' => $this->company_id];
-
-        $data['store'] = Storemodel::whereIn('id', $store_ids)->get(['id', 'name', 'province', 'city', 'district']);
-
-        $this->api_res(0, $data);
-    }
+class Login extends MY_Controller
+{
+	protected $app;
+	protected $oauth;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('employeemodel');
+		$this->load->helper('common');
+		$this->app = (new Application(getMiniWechatConfig()))->mini_program;
+	}
+	
+	public function getToken()
+	{
+		$this->load->library("wxbizdatacrypt");
+		$this->load->library('M_jwt');
+		$post = $this->input->post(NULL, true);
+		log_message('debug', json_encode($post));
+		if ($post['code']) {
+			$sessionKeyData = $this->app->sns->getSessionKey($post['code']);
+			$this->debug('debug','sessionKeyData为-->'.$sessionKeyData);
+			if (!isset($sessionKeyData->unionid)) {
+				$this->debug('debug','===未授权====');
+				$encryptedData = $post['encryptedData'];
+				$iv            = $post['iv'];
+				$pc            = new WXBizDataCrypt(config_item('miniAppid'), $sessionKeyData->sessionKey);
+				$data          = '';
+				$errCode       = $pc->decryptData($encryptedData, $iv, $data);
+				$data          = json_decode($data, true);
+				$this->debug('消息解密-->',$data);
+				if ($errCode == 0) {
+					$unionid = $data['unionid'];
+				} else {
+					$this->api_res('10103', $errCode);
+					return false;
+				}
+			}else{
+				$unionid = $sessionKeyData->unionid;
+			}
+			$wechat = Employeemodel::where('unionid', $unionid)
+				->where('status', 'ENABLE')
+				->first();
+			$this->debug('员工信息-->', $wechat);
+			if (empty($wechat)) {
+				$this->api_res(10002);
+				return false;
+			}
+			log_message('debug', '获取员工信息' . json_encode($wechat->toArray()));
+			$wechat->mini_openid = $sessionKeyData->openid;
+			$wechat->session_key = $sessionKeyData->session_key;
+			$wechat->save();
+			$token = $this->m_jwt->generateJwtToken($wechat->bxid, $wechat->company_id);
+			$this->api_res(0, ['token' => $token]);
+		} else {
+			log_message('error', 'mini-getToken-获取token失败');
+			$this->api_res(10101);
+		}
+	}
+	
+	public function authority()
+	{
+		$this->load->model('storemodel');
+		//获取门店列表
+		$store_ids = $this->employee_store->store_ids;
+		if (empty($store_ids) || !isset($store_ids)) {
+			$this->api_res(1018);
+			return;
+		}
+		$where         = ['company_id' => $this->company_id];
+		$data['store'] = Storemodel::whereIn('id', $store_ids)->get(['id', 'name', 'province', 'city', 'district']);
+		$this->api_res(0, $data);
+	}
 }
